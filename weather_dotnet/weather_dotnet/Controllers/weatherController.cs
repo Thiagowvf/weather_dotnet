0711f1da27b4df4a27a6fa8f9a4c29f4826bfba7
Thiago <thiagowvf@gmail.com>
2020-02-12 23:53:40 -0300

    Hello world da weather

---
+++ weather_dotnet/weather_dotnet/Controllers/weatherController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace weather_dotnet.Controllers
+{
+    public class weatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
